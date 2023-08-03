50cb08053c12546cfbd373c04cdbe885153a828c
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-08-04 11:47:02 +1200

    I have linked the page and added style

---
+++ src/lib/Nav.svelte
@@ -0,0 +1,12 @@
+<div class="mainImg">
+    <div class="navPos">
+      <div class="topnav">
+           
+        <a href="/">Home</a>
+        <a href="/about">About Us</a>
+        <a href="/drinks">Drink List</a>
+        <a href="/contact">Contact</a>
+        <a href="/blog">Blog</a>
+      </div>
+    </div>
+  </div>
