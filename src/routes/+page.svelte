541783de595ceefcfb27aa16d6d4fe61618a44bc
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-08-04 14:41:31 +1200

    I have added in a info text and an image of the owners

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,3 +1,9 @@
 <h1>SunSplash Beverages</h1>
-
-
+<div class="clear">
+    <div id="col1">
+            Founded in the summer of 2018, SunSplash Beverages emerged from the dream of two beach-loving friends, Alex and Maya. Their shared passion for tropical getaways, sandy shores, and refreshing drinks inspired them to create a brand that would capture the essence of summer in every sip. With a mission to bring the taste of sunshine to people's lives, they embarked on a journey to craft the most delightful and rejuvenating summer drinks. 
+    </div>
+    <div id="col2">
+        <img src="/img/2people.jpg">
+    </div>
+</div>
