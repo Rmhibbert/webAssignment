5708c6aa412e79672530ca54264c6f813d8348e0
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-08-04 12:13:22 +1200

    I have created a main img

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -1,7 +1,9 @@
+
+
+
 <div class="mainImg">
     <div class="navPos">
-      <div class="topnav">
-           
+      <div class="topnav">           
         <a href="/">Home</a>
         <a href="/about">About Us</a>
         <a href="/drinks">Drink List</a>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,3 @@
 <h1>SunSplash Beverages</h1>
 
+
