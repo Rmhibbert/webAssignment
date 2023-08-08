01f7a5fc0adfa1e8a7c21df801e1aefe14007815
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-08-09 08:41:33 +1200

    adding changes from weekend

--- src/lib/Nav.svelte
+++ src/lib/Nav.svelte
@@ -3,7 +3,8 @@
 
 <div class="mainImg">
     <div class="navPos">
-      <div class="topnav">           
+      <div class="topnav">
+        <img src="/img/logo.svg" class="logo" alt="This is an image of my logo">           
         <a href="/">Home</a>
         <a href="/about">About Us</a>
         <a href="/drinks">Drink List</a>
