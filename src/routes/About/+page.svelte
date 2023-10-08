761a878ed2d54a649b45330f3dfdd5c830b85162
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-10-08 19:04:01 +1300

    started on the structure of the about page

--- src/routes/About/+page.svelte
+++ src/routes/About/+page.svelte
@@ -1,1 +1,17 @@
+<svelte:head>
+    <link rel="stylesheet" href="css/main.css" />
+</svelte:head>
+
 <h1>About</h1>
+
+
+<div class="back">
+    <div class="aboutLeft"> </div>
+    <div class="aboutImg"> </div>
+</div>
+
+
+<div class="back">
+    <div class="aboutLeft aboutImg"></div>
+    <div class=""></div>
+</div>
