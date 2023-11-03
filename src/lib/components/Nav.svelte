fc4089975df6d699bb92ed7a162ab6fa10402468
Rory Hibbert <hibbrm1@student.op.ac.nz>
2023-11-03 13:16:50 +1300

    I have doen some more scalability

--- src/lib/components/Nav.svelte
+++ src/lib/components/Nav.svelte
@@ -10,20 +10,20 @@
 <nav id="sidebar-menu">
   <h3>Menu</h3>
   <ul>
-    <li><a href="/">Home</a></li>
-    <li><a href="/About">About Us</a></li>
-    <li><a href="/Drinks">Drink List</a></li>
-    <li><a href="/Gallery">Gallery</a></li>
-
+    <li><a href="/" target="_self">Home</a></li>
+    <li><a href="/About" target="_self">About Us</a></li>
+    <li><a href="/Drinks" target="_self">Drink List</a></li>
+    <li><a href="/Gallery" target="_self">Gallery</a></li>
+    <h4>EXIT</h4>
   </ul>
 </nav>
 </label>
         <div class="topnav">
           <img src="/img/logo.svg" class="logo" alt="This is an image of my logo">           
-          <a href="/" class = "trans">Home</a>
-          <a href="/About" class = "trans">About Us</a>
-          <a href="/Drinks" class = "trans">Drink List</a>
-          <a href="/Gallery" class = "trans">Gallery</a>      
+          <a href="/" >Home</a>
+          <a href="/About" >About Us</a>
+          <a href="/Drinks" >Drink List</a>
+          <a href="/Gallery" >Gallery</a>      
         </div>
     
 <div class="overlay"></div>
@@ -32,9 +32,3 @@
   </div> 
 </div>
 
-  <script>
-    function openFunction(){
-      
-
-  }
-  </script>
